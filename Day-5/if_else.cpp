0c33365d672205743b2c1d57bb3beaae077f8d6e
rushank7 <rushank.2008@gmail.com>
2025-01-21 18:02:57 +0100

    dsa new start

---
+++ Day-5/if_else.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int marks = 60;
+
+    if (marks > 70) {
+        cout << "Grade :F" <<endl;
+    } else if (marks >= 25 && marks <= 44) {
+        cout << "Grade : E" <<endl;
+        else if (marks = 50 && marks == 61) {
+            cout << "Grade C" <<endl;
+        else {cout << "Grade : << grade << std::endl";   
+        }
+    }
+    }
+
+    return 0;
+}
+
